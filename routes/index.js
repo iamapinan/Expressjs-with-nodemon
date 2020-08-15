fad31502355c56702930162a93fa54ea731a9251
Apinan Woratrakun <iamapinan@gmail.com>
2020-08-15 16:32:32 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express with Nodemon' });
+  res.render('index', { title: 'API for Air In One 1' });
 });
 
 module.exports = router;
